f4d17e366a4968edb3656d37b031c0971382edb8
Tim Morgan <github@timothymorgan.info>
2018-03-30 18:47:22 -0700

    Ruby and Rails updates

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-ApplicationController.renderer.defaults.merge!(
-    http_host: 'also.fm',
-    https:     true
-)
+ActiveSupport::Reloader.to_prepare do
+  ApplicationController.renderer.defaults.merge!(
+      http_host: 'also.fm',
+      https:     true
+  )
+end
